06e2aa41c5131407f279098db7e3ead042fb6b74
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-10-26 09:59:23 +0800

    Create optimized.cpp

---
+++ optimized.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	//ios::sync_with_stdio(false);
+	//cin.tie(0);
+	cout << 1 << '\n';
+	int x;
+	cin >> x;
+	cout << (x ^ 1) << '\n';
+	return 0;
+}
